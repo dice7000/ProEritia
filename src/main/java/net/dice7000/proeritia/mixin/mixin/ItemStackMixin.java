d5da7b920967634edae9e9b45563d11b9deb7a9b
dice7000 <noreply@abcdefggfedcba.aiueo>
2026-03-03 16:31:38 +0900

    Server friendly mode and some fix - 1.0.1

--- src/main/java/net/dice7000/proeritia/mixin/mixin/ItemStackMixin.java
+++ src/main/java/net/dice7000/proeritia/mixin/mixin/ItemStackMixin.java
@@ -41,7 +41,7 @@ public abstract class ItemStackMixin {
 
     @Inject(method = "is(Lnet/minecraft/world/item/Item;)Z", at = @At("HEAD"), cancellable = true)
     public void passItemIsInject(Item pItem, CallbackInfoReturnable<Boolean> cir) {
-        if (!proEritia$initialized) cir.setReturnValue(cir.getReturnValue());
+        if (!proEritia$initialized) return;
         if (proEritia$targetClass.getItem() instanceof PERArmor armor && armor.getMatterType() == PERMatterType.INF) {
             for (int i = 0; i < 3; i++) {
                 if (pItem == PERUtil.getSlot(proEritia$targetClass, i).getItem()) {
@@ -52,7 +52,7 @@ public abstract class ItemStackMixin {
     }
     @Inject(method = "is(Lnet/minecraft/tags/TagKey;)Z", at = @At("HEAD"), cancellable = true)
     public void passTagKeyIsInject(TagKey<Item> pTag, CallbackInfoReturnable<Boolean> cir) {
-        if (!proEritia$initialized) cir.setReturnValue(cir.getReturnValue());
+        if (!proEritia$initialized) return;
         if (proEritia$targetClass.getItem() instanceof PERArmor armor && armor.getMatterType() == PERMatterType.INF) {
             for (int i = 0; i < 3; i++) {
                 if (PERUtil.getSlot(proEritia$targetClass, i).getItem().builtInRegistryHolder().is(pTag)) {
@@ -63,7 +63,7 @@ public abstract class ItemStackMixin {
     }
     @Inject(method = "is(Ljava/util/function/Predicate;)Z", at = @At("HEAD"), cancellable = true)
     public void passPredicateIsInject(Predicate<Holder<Item>> pItem, CallbackInfoReturnable<Boolean> cir) {
-        if (!proEritia$initialized) cir.setReturnValue(cir.getReturnValue());
+        if (!proEritia$initialized) return;
         if (proEritia$targetClass.getItem() instanceof PERArmor armor && armor.getMatterType() == PERMatterType.INF) {
             for (int i = 0; i < 3; i++) {
                 if (pItem.test(PERUtil.getSlot(proEritia$targetClass, i).getItem().builtInRegistryHolder())) {
@@ -74,7 +74,7 @@ public abstract class ItemStackMixin {
     }
     @Inject(method = "is(Lnet/minecraft/core/Holder;)Z", at = @At("HEAD"), cancellable = true)
     public void passHolderIsInject(Holder<Item> pItem, CallbackInfoReturnable<Boolean> cir) {
-        if (!proEritia$initialized) cir.setReturnValue(cir.getReturnValue());
+        if (!proEritia$initialized) return;
         if (proEritia$targetClass.getItem() instanceof PERArmor armor && armor.getMatterType() == PERMatterType.INF) {
             for (int i = 0; i < 3; i++) {
                 if (pItem == PERUtil.getSlot(proEritia$targetClass, i).getItem().builtInRegistryHolder()) {
